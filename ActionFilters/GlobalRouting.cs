0b6ee85adadf5758e9baf642eb2f9b550b8a6db2
Chris O'Brien <chrisobrien2000@outlook.com>
2020-04-21 14:04:49 -0500

    Added logic for GlobalRouting class

--- ActionFilters/GlobalRouting.cs
+++ ActionFilters/GlobalRouting.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,18 @@ namespace TrashCollector.ActionFilters
 
         public void OnActionExecuting(ActionExecutingContext context)
         {
-            throw new NotImplementedException();
+            var controller = context.RouteData.Values["controller"];
+            if (controller.Equals("Home"))
+            {
+                if (_claimsPrincipal.IsInRole("Customer"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "Customers", null);
+                }
+                else if (_claimsPrincipal.IsInRole("Employee"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "Employees", null);
+                }
+            }
         }
     }
 }
