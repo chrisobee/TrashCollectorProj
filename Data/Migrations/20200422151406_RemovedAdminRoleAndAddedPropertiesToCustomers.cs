8a360d5431e9071de506102f11190b40814a5fe5
Chris O'Brien <chrisobrien2000@outlook.com>
2020-04-22 10:18:15 -0500

    Added Migration with new Properties for Customers

---
+++ Data/Migrations/20200422151406_RemovedAdminRoleAndAddedPropertiesToCustomers.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Data.Migrations
+{
+    public partial class RemovedAdminRoleAndAddedPropertiesToCustomers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "26f575f1 - ca97 - 4a58 - b87e - 69fc4907ef81");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Balance",
+                table: "Customers",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OneTimePickup",
+                table: "Customers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "PickupDay",
+                table: "Customers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "01c8a6c3-a16b-4bbc-9789-88db637c8778",
+                column: "ConcurrencyStamp",
+                value: "c01f5852-c243-4b6d-b7fd-2ffdefef917a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3e1ea929-a517-49d8-87bc-dec994a34e67",
+                column: "ConcurrencyStamp",
+                value: "fd4b77cf-f893-4be8-9b36-68e1e4a3312b");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "OneTimePickup",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "PickupDay",
+                table: "Customers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "01c8a6c3-a16b-4bbc-9789-88db637c8778",
+                column: "ConcurrencyStamp",
+                value: "e17753b0-5d9d-4cd1-acec-045dddf4c929");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3e1ea929-a517-49d8-87bc-dec994a34e67",
+                column: "ConcurrencyStamp",
+                value: "bcef994d-0d48-4d74-a34b-47f9cc61cf99");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "26f575f1 - ca97 - 4a58 - b87e - 69fc4907ef81", "c41fe56f-c882-4398-9993-011b23e36ec8", "Admin", "ADMIN" });
+        }
+    }
+}
